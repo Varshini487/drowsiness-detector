e02ef01c7fbdc70fc24b2634ab61dbda60004d34
Varshini487 <144593512+Varshini487@users.noreply.github.com>
2026-06-12 09:02:24 +0530

    Add app

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import cv2
+from scipy.spatial import distance
+
+st.title("Drowsiness Detector")
+
+def eye_aspect_ratio(eye):
+    A = distance.euclidean(eye[1], eye[5])
+    B = distance.euclidean(eye[2], eye[4])
+    C = distance.euclidean(eye[0], eye[3])
+    return (A + B) / (2.0 * C)
+
+run = st.checkbox("Start Monitoring")
+frame_window = st.image([])
+
+if run:
+    camera = cv2.VideoCapture(0)
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    while run:
+        ret, frame = camera.read()
+        if ret:
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            frame_window.image(frame)
+    camera.release()
